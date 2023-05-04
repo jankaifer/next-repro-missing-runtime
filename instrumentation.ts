016479cff64987a28c6ad17e9eb099fe2fb6cb4a
Jan Kaifer <jan@kaifer.cz>
2023-05-04 15:51:14 +0200

    added process import

--- instrumentation.ts
+++ instrumentation.ts
@@ -1,3 +1,5 @@
+import * as process from "process";
+
 export function register() {
   globalThis.message = `Message from instrumentation.ts: ${JSON.stringify({
     next_runtime: process.env.NEXT_RUNTIME,
