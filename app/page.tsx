b56fdf7f6c6bc5a2d00b6380d598901626170043
Jan Kaifer <jan@kaifer.cz>
2023-05-04 15:46:23 +0200

    reproduce

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,5 @@
+export const revalidate = 0;
+
 /** Add your relevant code here for the issue to reproduce */
 export default function Home() {
   return <div>global message:{globalThis.message}</div>;
--- instrumentation.ts
+++ instrumentation.ts
@@ -2,4 +2,5 @@ export function register() {
   globalThis.message = `Message from instrumentation.ts: ${JSON.stringify({
     next_runtime: process.env.NEXT_RUNTIME,
   })}`;
+  console.log("running instrumentation from", process.env.NEXT_RUNTIME);
 }
