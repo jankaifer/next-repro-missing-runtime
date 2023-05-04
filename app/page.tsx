6e4c6fa94a7272fdfae5db04f5a164cd4dc6adad
Jan Kaifer <jan@kaifer.cz>
2023-05-04 14:46:53 +0200

    reproduced

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,4 @@
 /** Add your relevant code here for the issue to reproduce */
 export default function Home() {
-  return null
+  return <div>{JSON.stringify({next_runtime: process.env.NEXT_RUNTIME})}</div>
 }
